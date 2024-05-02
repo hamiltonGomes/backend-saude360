20f88e318a38ee94e2d0af7b43f71b2ad16a0af4
Hamilton <hamilton.gomes8@hotmail.com>
2024-05-02 00:36:37 -0300

    refactor: Serialization adjustments for Address and User classes

--- src/main/java/com/saude360/backendsaude360/entities/Address.java
+++ src/main/java/com/saude360/backendsaude360/entities/Address.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
@@ -12,15 +13,13 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Address implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+
     private String cep;
     private String neighborhood;
     private String city;
     private String state;
     private int number;
     private String complement;
-
 }
--- src/main/java/com/saude360/backendsaude360/entities/User.java
+++ src/main/java/com/saude360/backendsaude360/entities/User.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Entity
@@ -13,7 +15,10 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected long id;
